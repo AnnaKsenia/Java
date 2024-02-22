59922baa06d0f37df1b61fd91fe304f00c156c3e
AnnaKsenia <145358343+AnnaKsenia@users.noreply.github.com>
2024-02-22 09:31:26 +0300

    Add files via upload

---
+++ java_01.js
@@ -0,0 +1,30 @@
+'Привет, Java!'
+var myThing;
+myThing=5;
+myThing="это строка";
+var numberNine = 9;
+var stringNine = "9";
+numberNine + numberNine;
+stringNine + stringNine;
+var greeting = "Привет, ";
+var myName = "Ник";
+greeting + myName;
+myName.length
+
+var name_orz = "ОРЗ ";
+var name_vetr = " Ветрянка ";
+
+var num_orz_mon = 2;
+var num_orz_tues = 3;
+var num_orz_wedn = 2;
+
+var num_vetr_tues = 1;
+var num_vetr_wedn = 4;
+
+num_orz_week = num_orz_mon + num_orz_tues + num_orz_wedn;
+num_vetr_week = num_vetr_tues + num_vetr_wedn;
+
+name_orz + num_orz_week;
+name_vetr + num_vetr_week;
+
+name_orz + num_orz_week + name_vetr + num_vetr_week
